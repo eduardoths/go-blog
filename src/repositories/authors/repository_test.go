d15e2c48f51664e512b63e301607fd5f5aa45496
Eduardo Santos <santos.eduardothomaz@gmail.com>
2021-11-12 13:02:58 -0300

    test: Update/delete non existing authors

--- src/repositories/authors/repository_test.go
+++ src/repositories/authors/repository_test.go
@@ -91,6 +91,7 @@ func TestUpdate(t *testing.T) {
 		expectedAuthor := structs.Author{
 			Name: "Test Author Two",
 			Email: "test2@author.com",
+
 		}
 		actualErr := authorRepo.Update(id, expectedAuthor) 
 		actualAuthor := queryAuthor(id)
@@ -113,8 +114,46 @@ func TestUpdate(t *testing.T) {
 		cleanTimestamp(&actualAuthor)
 		tests.AssertEquals(t, expectedErr, actualErr)
 		tests.AssertEquals(t, expectedAuthor, actualAuthor)
-
-
+	})
+	t.Run("Update already deleted author", func(t *testing.T) {
+		var expectedErr string = "record not found"
+		id, _ := insertAuthor()
+		db.Where("id = ?", id).Delete(&structs.Author{})
+		expectedAuthor := structs.Author {
+			ID: id,
+			Name: "Deleted user",
+			Email: "test@author.com",
+		}
+		actualErr := authorRepo.Update(id, expectedAuthor)
+		tests.AssertEquals(t, expectedErr, actualErr.Error())
+	})
+	t.Run("Update passing extra fields", func(t *testing.T) {
+		// I shouldn't be able to change metadata fields such as ID and CreatedAt
+		id, _ := insertAuthor()
+		fakeId := 4812
+		fakeCreatedAt := time.Date(2001, 12, 31, 23, 58, 58, 2, time.UTC)
+		var expectedErr error = nil
+		updatedAuthor := structs.Author {
+			ID: fakeId,
+			Name: "author",
+			Email: "author@author.com",
+			CreatedAt: fakeCreatedAt,
+		}
+		actualErr := authorRepo.Update(id, updatedAuthor)
+		actualAuthor := queryAuthor(id)
+		tests.AssertEquals(t, expectedErr, actualErr)
+		if actualAuthor.ID == fakeId {
+			t.Errorf("Author update is updating id")
+		}
+		if actualAuthor.CreatedAt == fakeCreatedAt {
+			t.Errorf("Author update is updating created_at")
+		}
+	})
+	t.Run("Update unexisting author", func(t *testing.T) {
+		fakeId := 4813
+		var expectedErr string = "record not found"
+		actualErr := authorRepo.Update(fakeId, structs.Author{})
+		tests.AssertEquals(t, expectedErr, actualErr.Error())
 	})
 }
 
@@ -127,5 +166,18 @@ func TestDelete(t *testing.T) {
 		cleanTimestamp(&actualAuthor)
 		tests.AssertEquals(t, expectedErr, actualErr)
 		tests.AssertEquals(t, structs.Author{}, actualAuthor)  // Should still exist as we're using soft deletes
+	})
+	t.Run("Delete already deleted author", func(t *testing.T) {
+		id, _ := insertAuthor()
+		var expectedErr string = "record not found"
+		db.Where("id = ?", id).Delete(&structs.Author{})
+		actualErr := authorRepo.Delete(id)
+		tests.AssertEquals(t, expectedErr, actualErr.Error())
+	})
+	t.Run("Delete unexistent author", func(t *testing.T) {
+		var expectedErr string = "record not found"
+		fakeId := 347823
+		actualErr := authorRepo.Delete(fakeId)
+		tests.AssertEquals(t, expectedErr, actualErr.Error())
 	})
 }
